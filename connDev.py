eaa71cfa0b73a6e2c9dc937f65f478d803c3d56f
Steve Clement <steve@localhost.lu>
2014-05-05 14:08:12 +0000

    - Updated grabdev to check for uuid's and write an XML for iDevices - connDev now grabs all (max 2) iDevices connected to system

--- connDev.py
+++ connDev.py
@@ -2,7 +2,30 @@
 
 from subprocess import *
 
+from plistlib import *
+
+fhCH = open('connectionHistory.txt','a')
+
 output=check_output("/home/pi/Desktop/code/fearMe/grabDev.sh", shell=True)
 
+attached_iDevices = str(check_output(["/usr/bin/idevice_id", "-l"])).rstrip().split('\\n')
+
 if output:
-	print(output)
+	print(output, file=fhCH)
+	fhCH.close()
+
+if attached_iDevices:
+	if ( len(attached_iDevices) > 2 ):
+		print("More than 1 iDevice detected")
+		iDev_1 = attached_iDevices[0][2:]
+		iDev_2 = attached_iDevices[1]
+
+		iDev1XML = Popen(["/home/pi/Desktop/code/fearMe/grabDev.sh", iDev_1], shell=False)
+
+		iDev2XML = Popen(["/home/pi/Desktop/code/fearMe/grabDev.sh", iDev_2], shell=False)
+
+		print(iDev_1)
+		print(iDev_2)
+	else:
+		iDev = attached_iDevices[0][2:]
+		iDevXML = Popen(["/home/pi/Desktop/code/fearMe/grabDev.sh", iDev], shell=False)
--- grabDev.sh
+++ grabDev.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
+if [ -z "${1}" ]; then
 sudo dmesg --facility=kern -c |grep -e 'usb 1' |grep Product: |awk '{for(i=6;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'
 # dmesg --facility=kern |grep -e 'usb 1' |grep Product: |awk '{for(i=6;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'
+else
+/usr/bin/ideviceinfo -x -s -u ${1} > xml/${1}.xml
+fi
