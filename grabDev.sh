1b169be9f18e9290d6cf05e5b23e747cf0f1ead3
Steve Clement <steve@localhost.lu>
2014-05-05 15:40:04 +0000

    - Separated iDevices from regular devices (by passing args) - Write xml file if iDevice present - Only output if usb device recently added (hacked via usb greppEage read: breakeage)

--- grabDev.sh
+++ grabDev.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-if [ -z "${1}" ]; then
-sudo dmesg --facility=kern -c |grep -e 'usb 1' |grep Product: |awk '{for(i=6;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'
-# dmesg --facility=kern |grep -e 'usb 1' |grep Product: |awk '{for(i=6;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'
-else
-/usr/bin/ideviceinfo -x -s -u ${1} > xml/${1}.xml
+usb_product=`dmesg --facility=kern |grep -e 'usb 1' |grep Product: |awk '{for(i=6;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'`
+
+if [ -n "${usb_product}" ]; then
+	echo -n "Product: "
+	dmesg --facility=kern |grep -e 'usb 1' |grep Product: |awk '{for(i=6;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'
+	echo -n "Manufacturer: "
+	dmesg --facility=kern |grep -e 'usb 1' |grep Manufacturer: |awk '{for(i=6;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'
+	sudo dmesg --facility=kern -c |grep -e 'usb 1' |grep idVendor |awk '{for(i=8;i<NF;i++)printf "%s",$i OFS; if (NF) printf "%s",$NF; printf ORS}'
+fi
+
+
+if [ -n "${1}" ]; then
+	/usr/bin/ideviceinfo -x -s -u ${1} > xml/${1}.xml
 fi
