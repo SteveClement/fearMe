55dcae4b420f33d4e30c8e6e97ed22b9224dc87b
Steve Clement <steve@localhost.lu>
2014-05-11 20:32:25 +0000

    - Added dupe device checks - Audio output - some LCD Debug foo

--- connDev.py
+++ connDev.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python3
 
+from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+
 from subprocess import *
 from plistlib import *
 from time import *
 from os.path import expanduser
+from datetime import datetime
 import os
 
+lcd = Adafruit_CharLCDPlate()
+lcd.begin(16,2)
 
+cmd = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+safetyMessage="Your device is very safe now."
+
+debug = {'glob': False, 'lcd': False }
+
+lcd_debug = False
+
+def run_cmd(cmd):
+        p = Popen(cmd, shell=True, stdout=PIPE)
+        output = p.communicate()[0]
+        return output
+
+def sayStuff(msg):
+	festival = "/usr/bin/festival"
+	festival_opts = "--tts"
+	festival_file = "/tmp/say.txt"
+	fh = open(festival_file, "w")
+	print(msg, file=fh)
+	fh.close()
+
+	check_output([festival, festival_opts, festival_file])
 
 def main():
-
-	setup()
+	ipaddr = setup()
+	#sayStuff("Welcome to FEAR ME")
+	#lcd.message('IP %s' % ( str(ipaddr) ) )
+	lcd.clear()
+	lcd.message('Analyzing phone...')
 	home = expanduser("~")
 	code = "/Desktop/code/fearMe"
 	grabDev = home + code + "/grabDev.sh"
@@ -18,19 +47,28 @@ def main():
 	fhCH = open('txt/connectionHistory.txt','a')
 
 	output=check_output(grabDev, shell=True)
+	print(output)
 
 	try:
 		attached_iDevices = str(check_output(["/usr/bin/idevice_id", "-l"])).rstrip().split('\\n')
 		iDev = True
-		print("Grabbing attached iDevices")
+		if debug['lcd']:
+			lcd.clear()
+			lcd.message("Grabbing attached iDevices")
 	except:
 		iDev = False
-		print("No iDevice connected, move along")
+		if debug['lcd']:
+			lcd.clear()
+			lcd.message("No iDevice connected, move along")
 
 	if output:
 		print(output, file=fhCH)
 		attachedDevices=str(output).rstrip().split('\\n')
 		print(attachedDevices[0][2:].split(":")[0] + ":" + attachedDevices[0][2:].split(":")[1])
+		attachedDevice = attachedDevices[0][2:].split(":")[1].replace("iPhone", "eye Phone")
+		attachedDevice = attachedDevices[0][2:].split(":")[1].replace("iPad", "eye Pad")
+		print(attachedDevice)
+		sayStuff("You connected your " + attachedDevice)
 		print(attachedDevices[1].split(":")[0] + ":" + attachedDevices[1].split(":")[1])
 		fhCH.close()
 
@@ -42,41 +80,79 @@ def main():
 
 			if ( os.path.isfile('xml/' + iDev_1 + '.xml') ):
 				print("We have this device already")
-				readDev(iDev_1)
+				iDevDupe = True
+				readDev(iDev_1, iDevDupe)
 			else:
 				iDev1XML = Popen([grabDev, iDev_1], shell=False)
-				readDev(iDev_1)
+				iDevDupe = False
+				readDev(iDev_1, iDevDupe)
 
 			if ( os.path.isfile('xml/' + iDev_2 + '.xml') ):
 				print("We have this device already")
-				readDev(iDev_2)
+				iDevDupe = True
+				readDev(iDev_2, iDevDupe)
 			else:
 				iDev2XML = Popen([grabDev, iDev_2], shell=False)
-				readDev(iDev_2)
+				iDevDupe = False
+				readDev(iDev_2, iDevDupe)
 
 		else:
 			iDev = attached_iDevices[0][2:]
 
 			if ( os.path.isfile('xml/' + iDev + '.xml') ):
 				print("We have this device already")
-				readDev(iDev)
+				iDevDupe = True
+				readDev(iDev, iDevDupe)
 			else:
 				iDevXML = Popen([grabDev, iDev], shell=False)
-				readDev(iDev)
+				iDevDupe = False
+				readDev(iDev, iDevDupe)
+	else:
+		sayStuff(safetyMessage)
 
-def readDev(iDev):
-	sleep(2)
-	iDev_plist = readPlist('xml/' + iDev + '.xml')
+def readDev(iDev, iDevDupe=False):
+	# Sleep to allow write to disk! (which is weird because the shell script is done and no file on disk)
+	while True:
+		try:
+			iDev_plist = readPlist('xml/' + iDev + '.xml')
+			break
+		except:
+			continue
+
 	if iDev_plist['DeviceColor'] == '#3b3b3c':
 		iDev_plist['DeviceColor'] = 'grey'
+	iDevName = iDev_plist['DeviceName'].replace("'", "")
+	iDevName = iDev_plist['DeviceName'].replace("’", "")
+	iDevName = iDevName.replace("iPhone", "eye Phone")
+	iDevName = iDevName.replace("iPad", "eye Pad")
+	print(iDevName)
+	iDevVersion = iDev_plist['ProductVersion']
+	iDevColor = iDev_plist['DeviceColor']
+
+	try:
+		iDevPad = iDevName.index("iPad")
+	except:
+		iDevPad = False
+
 	print('Device specs: {} {} {} {}'.format(iDev_plist['DeviceName'] , iDev_plist['ProductVersion'] , iDev_plist['DeviceColor'] , iDev_plist['HardwareModel']))
+	if not iDevDupe:
+		if iDevPad:
+			sayStuff("Wow, you own an eye Device, you are probably loaded, taking extra good care of your data.")
+			sayStuff("Your eye Pad is called " + iDevName + ". It is running version " + iDevVersion + " and the color is " + iDevColor)
+		else:
+			sayStuff("Wow, you own an eye Device, you are probably loaded, taking extra good care of your data.")
+			sayStuff("Your eye Phone is called " + iDevName + ". It is running version " + iDevVersion + " and the color is " + iDevColor)
+
+		sayStuff(safetyMessage)
 	checkVersion(iDev_plist['ProductVersion'])
 
 def setup():
+	ipaddr = run_cmd(cmd)
 	if not os.path.exists('xml'):
 		os.makedirs('xml')
 	if not os.path.exists('txt'):
 		os.makedirs('txt')
+	return ipaddr
 
 def checkVersion(iOSVer):
 	#v7 = "7.0.6"
@@ -89,4 +165,6 @@ def checkVersion(iOSVer):
 	if iOSVer == v7:
 		print("You are runing: " + v7)
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+	while True:
+		main()
