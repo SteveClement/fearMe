a3b0a922d832646d7fff318142b48a352ac8a9df
Steve Clement <steve@localhost.lu>
2014-05-13 06:44:29 +0000

    - Added run scripts and boot scripts

---
+++ Scripts/fearMe_tmux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SESSIONNAME="fearMe"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ] 
+ then
+    tmux new-session -s $SESSIONNAME -n main -d
+    tmux send-keys -t $SESSIONNAME "~/Desktop/code/fearMe/Scripts/runme.sh" C-m 
+fi
+
+#tmux attach -t $SESSIONNAME
